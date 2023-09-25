1ab7d7195b79580e30f35a62d54c37aebdcde28b
fatanabil <fatanabil63@gmail.com>
2023-09-26 01:26:25 +0700

    create: FAQ card

---
+++ resources/js/Components/FAQCard.jsx
@@ -0,0 +1,13 @@
+export default function FAQCard({ question, answer }) {
+    return (
+        <div className="collapse collapse-arrow border-2 bg-white">
+            <input type="radio" name="my-accordion-2" className="peer" />
+            <div className="collapse-title text-xl font-medium peer-checked:bg-curious-blue peer-checked:text-white">
+                {question}
+            </div>
+            <div className="collapse-content peer-checked:bg-curious-blue peer-checked:text-white">
+                <p>{answer}</p>
+            </div>
+        </div>
+    );
+}
