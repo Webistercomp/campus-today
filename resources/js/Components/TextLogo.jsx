9832392cb47fa6e5ab21106025e9c72ac20b7062
fatanabil <fatanabil63@gmail.com>
2023-09-25 15:11:22 +0700

    create: text logo comp

---
+++ resources/js/Components/TextLogo.jsx
@@ -0,0 +1,12 @@
+export default function TextLogo() {
+    return (
+        <p>
+            <span className="text-pelorous">C</span>
+            <span className="text-apple">A</span>
+            <span className="text-selective-yellow">M</span>
+            <span className="text-fuzzy-brown">P</span>
+            <span className="text-apple">U</span>
+            <span className="text-indigo">S</span> <span>TODAY</span>
+        </p>
+    );
+}
