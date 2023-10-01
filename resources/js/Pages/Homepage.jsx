1ec5b265ea9c7ad0c97a975f291c194fce69886e
fatanabil <fatanabil63@gmail.com>
2023-10-02 01:11:54 +0700

    fix: error

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -447,7 +447,7 @@ export default function Homepage(props) {
                         title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen
+                        allowFullScreen
                         className="mx-auto"
                     ></iframe>
                 </div>
