601543ed1f7268a9cec168a3c9dd24c37581d7af
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-10-14 00:40:38 +1100

    feat: add admin mrsc

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,8 @@ class DatabaseSeeder extends Seeder
             ParticipantSeeder::class,
             ArticleSeeder::class,
             InstagramFeedSeeder::class,
+
+            AdminSeeder::class,
         ]);
     }
 }
