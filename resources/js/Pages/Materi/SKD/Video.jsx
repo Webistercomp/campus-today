1be3aec5ba893f6c7d5bdd50416ab721c99f6541
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-10-16 23:31:21 +1100

    feat: add view for materi (react)

---
+++ resources/js/Pages/Materi/SKD/Video.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+function Video(props) {
+    return (
+        <>
+            <Head title="Untitled" />
+            <div className="flex flex-col h-screen w-screen bg-slate-400 text-white items-center justify-center">
+                <h1>Ini halaman materi skd video</h1>
+            </div>
+        </>
+    );
+}
+
+export default Video;
