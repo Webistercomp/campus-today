a9772d29d27a4b1186d1f6526611863aeb274373
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-10-19 11:30:35 +1100

    feat: adding admin lte

---
+++ resources/js/Pages/Admin/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+function Index(props) {
+    return (
+        <>
+            <Head title="Untitled" />
+            
+        </>
+    );
+}
+
+export default Index;
