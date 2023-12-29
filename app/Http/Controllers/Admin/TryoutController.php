333513b59f51d887d23ade190f5d993750d698b1
fatanabil <fatanabil63@gmail.com>
2023-12-29 10:42:26 +0700

    feat: add question for tryout when edit

--- app/Http/Controllers/Admin/TryoutController.php
+++ app/Http/Controllers/Admin/TryoutController.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 
 class TryoutController extends Controller
 {
-    function index() {
+    function index()
+    {
         $tryouts = Tryout::where('is_event', 0)->get();
-        foreach($tryouts as $tryout) {
+        foreach ($tryouts as $tryout) {
             $roles = '';
-            foreach(json_decode($tryout->roles) as $role) {
+            foreach (json_decode($tryout->roles) as $role) {
                 $roles .= Role::find($role)->name . ', ';
             }
             $tryout->roles = substr($roles, 0, -2);
@@ -25,13 +26,14 @@ class TryoutController extends Controller
         $user = Auth::user();
         $menu = Route::getCurrentRoute()->getName();
         $menu = explode('.', $menu)[1];
-        foreach($tryouts as $tryout) {
+        foreach ($tryouts as $tryout) {
             $tryout->jumlah_soal = $tryout->questions()->count();
         }
         return view('admin.tryout.index', compact('tryouts', 'user', 'menu'));
     }
 
-    function create() {
+    function create()
+    {
         $materialTypes = MaterialType::all();
         $groups = GroupType::all();
         $user = Auth::user();
@@ -41,7 +43,8 @@ class TryoutController extends Controller
         return view('admin.tryout.create', compact('user', 'menu', 'roles', 'materialTypes', 'groups'));
     }
 
-    function store(Request $request) {
+    function store(Request $request)
+    {
         $request->validate([
             'name' => 'required',
             'code' => 'required',
@@ -56,30 +59,36 @@ class TryoutController extends Controller
         $newTryout->time = $request->time;
         $newTryout->description = $request->description;
         $newTryout->is_event = 0;
-        if(!$request->has('active')) {
+        if (!$request->has('active')) {
             $newTryout->active = 0;
         }
         $newTryout->save();
         return redirect()->route('admin.tryout.index');
     }
-    
-    function show($id) {
+
+    function show($id)
+    {
         $tryout = Tryout::find($id);
         $roles = '';
-        foreach(json_decode($tryout->roles) as $role) {
+        foreach (json_decode($tryout->roles) as $role) {
             $roles .= Role::find($role)->name . ', ';
         }
         $tryout->roles = substr($roles, 0, -2);
+        $questions = Tryout::with('questions.answers')->find($id)->questions;
+        $tryout->questions = $questions;
         $user = Auth::user();
         $menu = Route::getCurrentRoute()->getName();
         $menu = explode('.', $menu)[1];
         return view('admin.tryout.show', compact('tryout', 'user', 'menu'));
     }
-    
-    function edit($id) {
+
+    function edit($id)
+    {
         $tryout = Tryout::find($id);
         $tryout->roles = implode(',', json_decode($tryout->roles));
         $materialTypes = MaterialType::all();
+        $questions = Tryout::with('questions.answers')->find($id)->questions;
+        $tryout->questions = $questions;
         $user = Auth::user();
         $menu = Route::getCurrentRoute()->getName();
         $menu = explode('.', $menu)[1];
@@ -87,14 +96,16 @@ class TryoutController extends Controller
         return view('admin.tryout.edit', compact('tryout', 'user', 'menu', 'roles', 'materialTypes'));
     }
 
-    function update(Request $request, $id) {
+    function update(Request $request, $id)
+    {
         $tryout = Tryout::find($id);
-        
+
         $tryout->update($request->all());
         return redirect()->route('admin.tryout.index');
     }
 
-    function delete($id) {
+    function delete($id)
+    {
         $tryout = Tryout::find($id);
         $tryout->delete();
         return redirect()->route('admin.tryout.index');
--- resources/views/admin/tryout/edit.blade.php
+++ resources/views/admin/tryout/edit.blade.php
@@ -76,6 +76,106 @@
                 <button type="submit" class="btn btn-primary ml-auto">Simpan</button>
             </div>
         </form>
+        <div class="col-md-6 d-flex align-items-center mt-4">
+            <h5>Daftar Soal</h5>
+        </div>
+        <form id="question-form" method="POST">
+            <ol id="question-list" type="1">
+                @foreach ($tryout->questions as $question)
+                <li class="mb-4">
+                    <input type="text" name="question_{{$question->id}}" id="question_{{$question->id}}" class="question form-control" value="{{$question->question}}">
+                    <p class="m-0">Pilihan jawaban: </p>
+                    <ol type="A" class="row row-cols-3">
+                        @foreach ($question->answers as $answer)
+                        <li class="pr-4"><input type="text" name="question_{{$question->id}}_answer_{{$answer->id}}" id="question_{{$question->id}}_answer_{{$answer->id}}" class="answer form-control" value="{{$answer->answer}}"></li>
+                        @endforeach
+                    </ol>
+                </li>
+                @endforeach
+            </ol>
+            <div class="d-flex mt-3 m-0 justify-content-end">
+                <button id="add-question" type="button" class="btn btn-warning">Tambah Soal</button>
+                <button id="save-questions" type="submit" class="btn btn-primary ml-2">Simpan Soal</button>
+            </div>
+        </form>
     </div>
 </div>
+<script>
+$(document).ready(function () {
+    let questions = {!!json_encode($tryout->questions->toArray())!!}
+    let question_counter = questions.at(-1).id + 1
+    let answer_counter = questions.at(-1).answers.at(-1).id + 1
+
+    $('#add-question').on('click', () => {
+        const questionWrapper = document.querySelector('#question-list');
+        const question = document.createElement("li")
+        const qInput = document.createElement("input")
+        const qText = document.createElement("p")
+        const answerWrapper = document.createElement("ol")
+
+        qInput.type = "text"
+        qInput.name = `question_${question_counter}`
+        qInput.setAttribute("placeholder", `Soal ke-${question_counter}`)
+        qInput.setAttribute("value", ``)
+        qInput.id = `question_${question_counter}`
+        qInput.classList.add("question", "form-control")
+        question.appendChild(qInput)
+
+        qText.innerText = "Pilihan jawaban: "
+        qText.classList.add("m-0")
+        question.appendChild(qText)
+
+        answerWrapper.type = "A"
+        answerWrapper.classList.add("row", "row-cols-3")
+        for (let i = 1; i <= 5; i++) {
+            const answer = document.createElement("li")
+            const answerInput = document.createElement("input")
+            answerInput.type = "text"
+            answerInput.name = `question_${question_counter}_answer_${answer_counter}`
+            answerInput.classList.add(`answer`, 'form-control')
+            answerInput.setAttribute("id", `question_${question_counter}_answer_${answer_counter}`)
+            answerInput.setAttribute("placeholder", `Jawaban ${i}`)
+            answerInput.setAttribute("value", ``)
+            answer.classList.add("pr-4")
+            answer.appendChild(answerInput)
+            answerWrapper.appendChild(answer)
+            answer_counter++
+        }
+        question.classList.add("mb-4")
+        question.appendChild(answerWrapper)
+        questionWrapper.appendChild(question)
+        question_counter++
+    })
+
+    $('#question-form').on('submit', (e) => {
+        e.preventDefault()
+
+        const questionForm = document.querySelector('#question-form')
+        const formData = new FormData(questionForm)
+        const questionData = []
+        const answerData = []
+        for (const [name, value] of formData){
+            if(name.includes("answer")) {
+                answerData.push({name, value})
+            } else {
+                questionData.push({name, value})
+            }
+        }
+        const data = questionData.map(q => {
+            return {id: parseInt(q.name.replace("question_", "")), question: q.value, answers: answerData.filter(ans => ans.name.includes(q.name)).map(ans => {return {id: parseInt(ans.name.replace(`${q.name}_answer_`, "")), name: ans.name, answer: ans.value, question_id: parseInt(q.name.replace("question_", ""))}})}
+        })
+        console.log(data);
+        // This code below is for send data to update question route
+        // const route = "{!! route("admin.home") !!}"
+        // $.ajax({
+        //     type: "POST",
+        //     url: route,
+        //     data: data,
+        //     dataType: "application/json",
+        //     success: function (response) {
+        //     }
+        // });
+    })
+});
+</script>
 @endsection
