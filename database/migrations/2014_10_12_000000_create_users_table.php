5acbe063d464261e01b3cd79d091bc0df2dac5a7
fatanabil <fatanabil63@gmail.com>
2024-03-04 08:20:57 +0700

    fix: add picture field to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->integer('role_id');
             $table->date('expired_at')->nullable();
             $table->string('name');
+            $table->string('picture')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
