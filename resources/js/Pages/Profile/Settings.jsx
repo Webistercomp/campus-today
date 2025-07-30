30b7780e4fe499a46d06e966aa2e8779b275a616
Fata Nabil Fikri <fatanabil88@gmail.com>
2025-07-30 22:00:15 +0700

    init

--- resources/js/Pages/Profile/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
-
-export default function Settings({ setAlertData }) {
-    return (
-        <>
-            <UpdatePasswordForm setAlertData={setAlertData} />
-        </>
-    );
-}
--- testniaga.sh
+++
@@ -1,1 +0,0 @@
-echo "Halo, duniaaaaa"
