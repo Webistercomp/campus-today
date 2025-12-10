d4063b388706b2570cb8d4b04178760b591d48fc
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2025-12-11 00:11:24 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
         }),
         react(),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'fortunate-vitality-production.up.railway.app'
+        }
+    }
 });
